ae2e0d8f0cf5702fbd73cdc7a8f3aeebc0eaf493
“cooks.johns” <cooks.johns@gmail.com>
2024-04-12 15:18:22 -0500

    Learing more about using ArrayList and Collections features as well as keySet

--- src/main/java/org/Interview/drills/leet/CloseStrings.java
+++ src/main/java/org/Interview/drills/leet/CloseStrings.java
@@ -8,34 +8,60 @@ public class CloseStrings {
 
     public boolean similarStrings(String word1, String word2) {
 
-        if ( word1.length() != word2.length()) {
+        // Check if the inputs are not the same lenght then end
+        if(word1.length() != word2.length()){
             return false;
         }
+
 //    Store values after looping
-        Map<Character, Integer> word1Map = new HashMap<>();
-        Map<Character, Integer> word2Map = new HashMap<>();
+        Map<Character, Integer> wordMap1 = new HashMap<>();
+        Map<Character, Integer> wordMap2 = new HashMap<>();
 
 //        Phase 1
-        for(int c : word1.toCharArray()) {
-            word1Map.put(c, word1Map.getOrDefault(c, 0) + 1);
+        for(char c : word1.toCharArray()) {
+            wordMap1.put(c, wordMap1.getOrDefault(c, 0) + 1);
         }
 
-        for(int c : word2.toCharArray()) {
-            word2Map.put(c, word2Map.getOrDefault(c, 0) + 1);
+        for(char c : word2.toCharArray()) {
+            wordMap2.put(c, wordMap2.getOrDefault(c, 0) + 1);
         }
 
 //        Phase 2
-        if (!word1Map.keySet().equals(word2Map.keySet())) {
+        if(!wordMap1.keySet().equals(wordMap2.keySet())){
             return false;
         }
 
-        List<Integer> word1FrenquencyList = new ArrayList<>();
-        List<Integer> word2FrenquencyList = new ArryList<>();
 
-        Collections.sort(word1FrenquencyList);
-        Collections.sort(word2FrenquencyList);
+        List<Integer> frequentWordList1 = new ArrayList<>(wordMap1.values());
+        List<Integer> frequentWrodList2 = new ArrayList<>(wordMap2.values());
 
-        return word1FrenquencyList.equals(word2FrenquencyList);
+
+        Collections.sort(frequentWordList1);
+        Collections.sort(frequentWrodList2);
+
+        return frequentWordList1.equals(frequentWrodList2);
+
+    }
+
+    public static void main(String[] args) {
+
+        public static void main(String[] args) {
+            String word1 = "abc";
+            String word2 = "bca";
+            CloseStrings solution = new CloseStrings();
+            boolean result = solution.similarStrings(word1, word2);
+            System.out.println("Example 1: " + result);
+
+            word1 = "a";
+            word2 = "aa";
+            result = solution.similarStrings(word1, word2);
+            System.out.println("Example 2: " + result);
+
+            word1 = "cabbba";
+            word2 = "abbccc";
+            result = solution.similarStrings(word1, word2);
+            System.out.println("Example 3: " + result);
+        }
 
     }
 
